32e5c59b4ee57197176947b8dc3e3f2db08e6e8f
Aleksandar <47703759+al3k5andar@users.noreply.github.com>
2022-03-06 15:48:43 +0100

    started implementing security

---
+++ src/test/java/spring/gameachievementsapi/controllers/api/v1/AchievementControllerTest.java
@@ -0,0 +1,29 @@
+package spring.gameachievementsapi.controllers.api.v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AchievementControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAchievementById() {
+    }
+
+    @Test
+    void createAchievement() {
+    }
+
+    @Test
+    void updateAchievement() {
+    }
+
+    @Test
+    void deleteAchievement() {
+    }
+}
