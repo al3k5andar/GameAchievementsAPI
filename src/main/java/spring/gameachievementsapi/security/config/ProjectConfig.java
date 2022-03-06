13cf971c0d4cc66ebbec062495bc750c53e7466b
Aleksandar <47703759+al3k5andar@users.noreply.github.com>
2022-03-06 20:15:04 +0100

    adding filter for authentication

---
+++ src/main/java/spring/gameachievementsapi/security/config/ProjectConfig.java
@@ -0,0 +1,50 @@
+package spring.gameachievementsapi.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import spring.gameachievementsapi.security.filters.CustomAuthenticationFilter;
+import spring.gameachievementsapi.security.providers.CustomAuthenticationProvider;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomAuthenticationFilter filter;
+    private final CustomAuthenticationProvider provider;
+
+    public ProjectConfig(CustomAuthenticationFilter filter, CustomAuthenticationProvider provider) {
+        this.filter = filter;
+        this.provider = provider;
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(provider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        http.addFilterAt(filter, BasicAuthenticationFilter.class);
+
+
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.authorizeRequests().anyRequest().permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+}
---
+++ src/main/java/spring/gameachievementsapi/security/filters/CustomAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package spring.gameachievementsapi.security.filters;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import spring.gameachievementsapi.security.authentication.CustomAuthentication;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component
+public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private Logger logger= Logger.getLogger(CustomAuthenticationFilter.class.getName());
+
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        logger.info("Searching for authorization!!!");
+
+        String theKey= request.getHeader("Authorization");
+
+        var authentication= new CustomAuthentication(theKey, null);
+
+        try {
+            Authentication result = authenticationManager.authenticate(authentication);
+
+            if (result.isAuthenticated()) {
+                SecurityContextHolder.getContext().setAuthentication(result);
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            }
+        }
+        catch (AuthenticationException e){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getServletPath().equals("/h2-console/**");
+    }
+}
